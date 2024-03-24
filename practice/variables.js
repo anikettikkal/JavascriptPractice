a803787518282b3217b5dac4b6b14106f6aa1f12
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-24 19:35:22 +0530

    variables folder created

---
+++ practice/variables.js
@@ -0,0 +1,4 @@
+/*variables in JS */
+console.log("there are three types of variables in JS i.e. const , var , let")
+/* prefer not to use var 
+because of issue in block scope and functional scope */
