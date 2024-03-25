c241e35e3ada1780214912898df0e2697fe5736b
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-25 21:24:39 +0530

    variables declare

--- practice/datatypes.js
+++ practice/datatypes.js
@@ -1,1 +1,10 @@
 /*datatypes in javascript*/
+"usescript"; //treat all js code as a newer version
+
+let age= 18
+let name = "aniket"
+let city = 'Deolali Pravara'
+let no = 8/2==4;
+studentId= "";
+
+
