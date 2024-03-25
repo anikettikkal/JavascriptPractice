32045c179509a253eb159542a8e598cf43b10d87
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-25 21:38:30 +0530

    exaples of datatypes added

--- practice/datatypes.js
+++ practice/datatypes.js
@@ -5,6 +5,21 @@ let age= 18
 let name = "aniket"
 let city = 'Deolali Pravara'
 let no = 8/2==4;
-studentId= "";
+studentId= 
 
+//number=> 2 to power 53
+//bigint => 251637859037294892
+//string=""
+//boolean=> true / false
+//null=> standalone value
+//undefined=> we declared variable but value not assigned
+//symbol=> unique
+//object
 
+// type cheacking
+console.log(typeof "aniket");
+console.table(typeof[age , name , city, no , studentId]) // output of this show object
+console.log(typeof name);
+console.log(typeof city);
+console.log(typeof no);
+console.log(typeof studentId);
