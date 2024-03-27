25e499c9086bf3daf889e5beaff549e79e1e455b
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-27 22:15:54 +0530

    second example added

--- practice/typeconversion-operations.js
+++ practice/typeconversion-operations.js
@@ -1,6 +1,11 @@
 // type conversion in js
 
 let value="aniket"
-console.log(value);
+//console.log(value);
 let doubleValue= Number(value)
-console.log(typeof doubleValue);
+//console.log(typeof doubleValue);
+
+let score= "33abc"
+let scoreValue= Number(score)
+console.log(scoreValue);
+console.log(typeof scoreValue); // print NaN i.e. not a number
