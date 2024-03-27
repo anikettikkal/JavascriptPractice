f5505ad3c911eb7f856d5da6c16f7fc19c730619
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-27 22:25:58 +0530

    type conversion completed

--- practice/typeconversion-operations.js
+++ practice/typeconversion-operations.js
@@ -7,5 +7,25 @@ let doubleValue= Number(value)
 
 let score= "33abc"
 let scoreValue= Number(score)
-console.log(scoreValue);
-console.log(typeof scoreValue); // print NaN i.e. not a number
+//console.log(scoreValue);
+//console.log(typeof scoreValue); // print NaN i.e. not a number
+
+let input= true
+let input1= false
+let output1= Number(input1)
+let output= Number(input)
+//console.log(output1);
+//console.log(output);
+
+let isLogin= 1
+let isLogout= Boolean(isLogin)
+console.log(isLogout);
+
+let name='aniket'
+console.log(Boolean(name));
+
+let no=""
+console.log(Boolean(no));
+
+/* 1=> true ; 0 => false;
+    "" => false ; "aniket" => true */
