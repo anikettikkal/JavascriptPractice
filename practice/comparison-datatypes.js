410359f8305b3363cc6b50b9e7f72a120d4ab977
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-29 22:46:02 +0530

    comparisons of data types completed

--- practice/comparison-datatypes.js
+++ practice/comparison-datatypes.js
@@ -2,9 +2,27 @@
 
 
 //1. basic camparisons
-// console.log(1>2);
-// console.log(1>=2);
-// console.log(1<2);
-// console.log(1<=2);
-// console.log(1==2);
-// console.log(1!=2); 
+console.log(1>2);
+console.log(1>=2);
+console.log(1<2);
+console.log(1<=2);
+console.log(1==2);
+console.log(1!=2);
+
+console.log("2" > 1); // for compare two values there is need to both having same datatypes
+
+// compare with null
+console.log(null>0);
+console.log(null<0);
+console.log(null<=0);
+
+// compare with undefined
+console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined<0);
+
+// strict cheack
+
+const score= 23
+const scoreValue= 35
+console.log(score=== scoreValue);
