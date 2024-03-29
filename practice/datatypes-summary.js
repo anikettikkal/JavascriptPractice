e53dae603e46599e5e4d7b7f8c956ff5deca394d
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-29 22:51:03 +0530

    data types summary file created

---
+++ practice/datatypes-summary.js
@@ -0,0 +1,5 @@
+/*there are two types of datatypes i.e. primitive or non-primitive
+    they are divided according to way of accesing data from memory
+*/
+
+// primitive (call by value) => 7 Category
--- practice/typeconversion-operations.js
+++ practice/typeconversion-operations.js
@@ -1,31 +1,31 @@
 // type conversion in js
 
 let value="aniket"
-//console.log(value);
+console.log(value);
 let doubleValue= Number(value)
-//console.log(typeof doubleValue);
+console.log(typeof doubleValue);
 
 let score= "33abc"
 let scoreValue= Number(score)
-//console.log(scoreValue);
-//console.log(typeof scoreValue); // print NaN i.e. not a number
+console.log(scoreValue);
+console.log(typeof scoreValue); // print NaN i.e. not a number
 
 let input= true
 let input1= false
 let output1= Number(input1)
 let output= Number(input)
-//console.log(output1);
-//console.log(output);
+console.log(output1);
+console.log(output);
 
 let isLogin= 1
 let isLogout= Boolean(isLogin)
-//console.log(isLogout);
+console.log(isLogout);
 
 let name='aniket'
-//console.log(Boolean(name));
+console.log(Boolean(name));
 
 let no=""
-//console.log(Boolean(no));
+console.log(Boolean(no));
 
 /* 1=> true ; 0 => false;
     "" => false ; "aniket" => true 
@@ -33,20 +33,20 @@ let no=""
 
 //------------------------------------ Operations Starts---------------------------------------------------
 
-//console.log(2+2);
-//console.log(2*2);
-//console.log(2-2);
-//console.log(2/2);
-//console.log(2%2);
-//console.log(2**3);
-//console.log(2*2+4);
-//console.log("1" + 2+2); // output is 122
-//console.log(2+2+"4"); // output is 44
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+console.log(2*2+4);
+console.log("1" + 2+2); // output is 122
+console.log(2+2+"4"); // output is 44
 
 let str1= "hello"
 let str2= "aniket"
 let str3= str1+ str2
-//console.log(str3);
+console.log(str3);
 
 let addition= 234
 console.log(++addition); // prefix 
