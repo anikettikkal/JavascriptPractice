a1e59d7a883936af6bf8b870f5919b31c28dc4c5
anikettikkal <tikkalaniket2019@gmail.com>
2024-03-29 22:36:57 +0530

    basic comparisons added

--- practice/comparison-datatypes.js
+++ practice/comparison-datatypes.js
@@ -1,1 +1,10 @@
 // comparisons of datatypes
+
+
+//1. basic camparisons
+// console.log(1>2);
+// console.log(1>=2);
+// console.log(1<2);
+// console.log(1<=2);
+// console.log(1==2);
+// console.log(1!=2); 
