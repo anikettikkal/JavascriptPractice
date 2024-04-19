359903407930e2e9752d947be9843402180db045
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-19 22:24:34 +0530

    basic methods completed

--- practice/stringmethods.js
+++ practice/stringmethods.js
@@ -1,3 +1,15 @@
 const name="aniket"
 const repoCount= 30
-console.log(`hello my self ${name} and my repo count i9s ${repoCount}`);
+//console.log(`hello my self ${name} and my repo count i9s ${repoCount}`);
+
+const gameName = new String("aniket-RT")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("t"));
+
+const newString= gameName.substring(0,4)
+console.log(newString);
