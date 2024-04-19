195a4fff47748dea0509a0ba55e9db336297dc17
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-19 22:31:59 +0530

    methods of string completed

--- practice/stringmethods.js
+++ practice/stringmethods.js
@@ -1,15 +1,28 @@
 const name="aniket"
 const repoCount= 30
-//console.log(`hello my self ${name} and my repo count i9s ${repoCount}`);
+console.log(`hello my self ${name} and my repo count i9s ${repoCount}`);
 
 const gameName = new String("aniket-RT")
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.toLowerCase());
-// console.log(gameName.charAt(4));
-// console.log(gameName.indexOf("t"));
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf("t"));
 
 const newString= gameName.substring(0,4)
 console.log(newString);
+
+const anotherString= gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "   aniket   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://aniket.com/aniket%20tikkal"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('aniket'));
+
+console.log(gameName.split('-'));
