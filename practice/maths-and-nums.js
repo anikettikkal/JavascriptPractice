2e3164532cd95fec70d63211afd4f0d98ad4813d
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-22 22:01:31 +0530

    maths methods start now

--- practice/maths-and-nums.js
+++ practice/maths-and-nums.js
@@ -5,12 +5,16 @@ console.log(balance);
 
 // methods for number ************************************
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber= 123.476
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));
 
 const hundreds= 1000000
-console.log(hundreds.toLocaleString());
-console.log(hundreds.toLocaleString('en-In'));
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-In'));
+
+// *******************************methods for maths ************************************************
+
+console.log(Math);
