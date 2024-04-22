bae4020bc7b9fa2589380e67fcf7c0f33ab6dfa9
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-22 21:57:46 +0530

    numbers methods complete

--- practice/maths-and-nums.js
+++ practice/maths-and-nums.js
@@ -1,4 +1,16 @@
 const score = 200
-const anotherScore= new Number(200)
+const balance= new Number(200)
 console.log(score);
-console.log(anotherScore);
+console.log(balance);
+
+// methods for number ************************************
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber= 123.476
+console.log(otherNumber.toPrecision(4));
+
+const hundreds= 1000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-In'));
