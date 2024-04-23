02e4e028b55c7ca04e98fc262f0de96e08ba0a55
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-23 22:29:48 +0530

    date and time all methods complete

--- practice/date-and-time.js
+++ practice/date-and-time.js
@@ -1,3 +1,12 @@
 let myDate= new Date()
 console.log(myDate.toString());
 console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMinutes());
+console.log(myDate.getMonth());
+console.log(myDate.getSeconds());
+console.log(myDate.getTime());
+console.log(myDate.getTimezoneOffset());
