fe8a52b7f1d20655e81a6dd58d7fbeee612148f1
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-25 22:35:03 +0530

    objects part1 completed

--- practice/objects-part1.js
+++ practice/objects-part1.js
@@ -1,4 +1,54 @@
 // declaration of objects
 
-const myObj = {}
-console.log(myObj);
+const myObj = {
+    name: "aniket",
+    age: 21,
+    location: "Ahilyanagar",
+    email: "aniket@gmail.com",
+    isLogedIn: false,
+    lastLogedIn: ["monday","friday"]
+}
+
+// console.log(myObj);
+// console.log(myObj["age"]);
+// console.log(myObj["lastLogedIn"]);
+
+// interview ask que ==> hpw to add symbol values in objects
+
+const symKey = Symbol('key1')
+
+const userObj = {
+    name: "aniket",
+    [symKey] : 1234,
+    age: 21,
+    location: "Ahilyanagar",
+    email: "aniket@gmail.com",
+    isLogedIn: false,
+    lastLogedIn: ["monday","friday"]
+}
+// console.log(userObj);
+// console.log(typeof userObj[symKey]);
+
+// how to change values
+
+// console.log(userObj.email="aniket03@gmail.com");
+// console.log(userObj);
+
+// how to lock or freeze the values in objects
+
+// Object.freeze(userObj)
+userObj.age=23
+// console.log(userObj);
+
+// adding functions in objects
+
+userObj.greeting = function() {
+    console.log("hello JsUser");
+}
+
+userObj.greetingTwo = function() {
+    console.log(`hello jsUser ${this.name}... Good Morning!`);
+}
+
+console.log(userObj.greeting());
+console.log(userObj.greetingTwo());
