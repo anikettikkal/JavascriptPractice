194d96223bfb59a1b4dce549120b6509cafad3fc
anikettikkal <tikkalaniket2019@gmail.com>
2024-04-25 22:47:28 +0530

    object part2 some part remaining

--- practice/objects-part2.js
+++ practice/objects-part2.js
@@ -1,1 +1,15 @@
 // objects part 2 starts now
+
+const realUser = {
+    email: "aniket@chatgpt.com",
+    fullname: {
+        userfullname:{
+            userfirstname: "aniket",
+            userlastname: "tikkal"
+        }
+    },
+    id: 123
+}
+
+console.log(realUser);
+console.log(realUser.fullname.userfullname.userfirstname);
