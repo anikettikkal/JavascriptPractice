d06f390f31c564554c5d73dcbeca154f24a9585a
anikettikkal <tikkalaniket2019@gmail.com>
2024-05-28 22:29:54 +0530

    function file creating and adding one programe

---
+++ functions/function.js
@@ -0,0 +1,10 @@
+//------------------ defining a function ---------------//
+function sayMyName () {
+    console.log("A");
+    console.log("N");
+    console.log("I");
+    console.log("K");
+    console.log("E");
+    console.log("T");
+}
+sayMyName()
