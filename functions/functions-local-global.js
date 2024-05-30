b503d73909784755bf28c3e0c768936a5ee4c986
anikettikkal <tikkalaniket2019@gmail.com>
2024-05-30 21:54:30 +0530

    function local and global scope ends

--- functions/functions-local-global.js
+++ functions/functions-local-global.js
@@ -10,4 +10,17 @@ if(true){
 // console.log(a);
 // console.log(b);
 // console.log(c);
+user("aniket")
 
+function user(anyObjectUser){
+    console.log( `hello ${anyObjectUser} how are you?`);
+    
+    userName("tikkal")
+    function userName(anyUser){
+        console.log(`${anyObjectUser} and ${anyUser} user is only for this block i.e local scope`);
+    }
+    // userName("tikkal")
+    // console.log(`${anyObjectUser} and ${anyUser} user is only for this block i.e local scope`);
+}
+
+// user("aniket")
