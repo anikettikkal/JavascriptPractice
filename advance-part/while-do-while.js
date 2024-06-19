e022c1cfd1bace8b61ed025b3799f4a5a69156ef
anikettikkal <tikkalaniket2019@gmail.com>
2024-06-19 10:15:40 +0530

    while doWhile loop ends

--- advance-part/while-do-while.js
+++ advance-part/while-do-while.js
@@ -5,3 +5,17 @@ while(index<=10){
     console.log(`even numbers between 0 to 10 is ${index}`);
     index=index+2;
 }
+
+
+let score=1
+do{
+    console.log(`score is = ${score}`);
+    score=score+3
+
+}while(score<=10)
+
+let marks=0
+do{
+    console.log(marks);
+    marks= marks+2
+} while(marks<=100)
