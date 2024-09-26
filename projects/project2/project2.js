79431e1323d9546a4fc3654ce36ec21798936476
anikettikkal <tikkalaniket2019@gmail.com>
2024-09-26 22:44:00 +0530

    second project final results added according to my knowledge

--- projects/project2/project2.js
+++ projects/project2/project2.js
@@ -9,6 +9,7 @@ form.addEventListener('submit', function(e){
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
+    const finalresults = document.querySelector('#finalresults')
 
 
     if(height=== '' || height< 0 || isNaN(height)){
@@ -19,5 +20,19 @@ form.addEventListener('submit', function(e){
         const bmi = (weight / ((height*height)/10000)).toFixed(2)
         // show the results
         results.innerHTML = `<span>${bmi}</span>`
+
+        // show final results
+        if(bmi< 18.6){
+            finalresults.innerHTML = "you are under weight"
+        }
+        else if(bmi>=18.6 && bmi<=24.9){
+            finalresults.innerHTML= "you are in normal range"
+        } else if(bmi>25){
+            finalresults.innerHTML= "you are overweighted"
+        } else{
+            finalresults.innerHTML="there is no need to explain you anything"
+        }
     }
+
+    
 })
