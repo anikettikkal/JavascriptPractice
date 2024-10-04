c0b85bea3acba38040f043314e9ffcf48ce2bf94
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-04 22:19:56 +0530

    all query selector in project can be selected

--- projects/project4/guess.js
+++ projects/project4/guess.js
@@ -1,3 +1,19 @@
 // how to genertae a random number
 
-console.log(parseInt(Math.random()*100+1));
+const randomNumber = (parseInt(Math.random()*100+1));
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p')
+
+
+
+
+
+
+
