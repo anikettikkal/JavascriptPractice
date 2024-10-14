0c23dc9a18bb24b05430ac10775ba8e6512baa75
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-14 22:35:54 +0530

    both start and stop button working properly

--- projects/unlimitedColors/colors.js
+++ projects/unlimitedColors/colors.js
@@ -1,9 +1,9 @@
 // generate a random color
 
-const randomColor = function(){
+const randomColor = function () {
     const hex = '123456789ABCDEF'
     let color = '#'
-    for(let i=0 ; i< 6; i++){
+    for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)]
     }
     return color;
@@ -11,20 +11,23 @@ const randomColor = function(){
 
 console.log(randomColor());
 
-const stopChangingColor = function(){
-    
-}
+let intervalId;
+const startChangingColor = function () {
+    intervalId = setInterval(changeBgColor, 2000)
 
-const startChangingColor = function(){
-    setInterval(changeBgColor,2000)
-
-    function changeBgColor(){
+    function changeBgColor() {
         document.body.style.backgroundColor = randomColor();
     }
 
     // background color changes
     // document.body.style.backgroundColor = randomColor();
 }
+
+const stopChangingColor = function () {
+    clearInterval(intervalId);
+}
+
+
 
 document.querySelector('#stop').addEventListener('click', stopChangingColor)
 document.querySelector('#start').addEventListener('click', startChangingColor)
