d5b2e03523e2b8e9c94ce14f846a613cf2103aea
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-17 22:11:04 +0530

    promise created and consumed

--- advance-part-api/promises.js
+++ advance-part-api/promises.js
@@ -1,2 +1,16 @@
 // how to create promises
-const promiseOne = new Promise
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    },1000)
+})
+
+// consume promise
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+    
+})
