857f453b776577d307929e114c3d9bdff6efc48b
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-24 21:48:44 +0530

    all promises completed today

--- advance-part-api/promises.js
+++ advance-part-api/promises.js
@@ -94,16 +94,25 @@ async function consumePromise() {
 consumePromise()
 
 
-async function getAllUSers() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await response.json()
-        console.log(data);
-    } catch (error) {
-        console.log('Err: error');
+// async function getAllUSers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log('Err: error');
         
-    }
+//     }
     
-}
+// }
 
-getAllUSers()
+// getAllUSers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
