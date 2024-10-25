e7a8b214d3582ed9269ee5e0ee15cb936a7c92f0
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-25 21:40:54 +0530

    promise four practise

--- selfProjects/sproject2/random.js
+++ selfProjects/sproject2/random.js
@@ -38,3 +38,31 @@ promiseThree.then(function(user){
     console.log(user);
     
 })
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({
+                username:"aniket",
+                password: "123"
+            })
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+    
+}).catch((error) => {
+    console.log(error);
+    
+}).finally(() => {
+    console.log("The Promise is either resolved or rejected");
+    
+})
