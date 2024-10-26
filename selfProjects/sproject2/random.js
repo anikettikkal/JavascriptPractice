119b41f2ab6dcbbb09634dd3d65dbf7856eafb65
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-26 21:06:31 +0530

    fetch and async function repeated

--- selfProjects/sproject2/random.js
+++ selfProjects/sproject2/random.js
@@ -115,3 +115,40 @@ fetch('https://api.github.com/users/hiteshchoudhary')
     console.log(data);
 })
 .catch((error) => console.log(error))
+
+
+async function consumePromise() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+consumePromise()
+
+async function getAllUSers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log('Err: error');
+        
+    }
+    
+}
+
+getAllUSers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
