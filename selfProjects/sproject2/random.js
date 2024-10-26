2457d9aa743d687f8e2ebcb6df0aa59a76f04076
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-26 21:05:35 +0530

    fetch method practice

--- selfProjects/sproject2/random.js
+++ selfProjects/sproject2/random.js
@@ -106,3 +106,12 @@ async function getAllUSers() {
 }
 
 getAllUSers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
