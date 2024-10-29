764564969781f24f5177a6b4d69a95319c7b9084
anikettikkal <tikkalaniket2019@gmail.com>
2024-10-29 22:22:12 +0530

    using this keyword

--- classes-and-oop/OOP.js
+++ classes-and-oop/OOP.js
@@ -1,8 +1,22 @@
 // Object Literal
 const user = {
-    username: 'Aniket',
-    loginCount: '3',
-    signedIn: 'truth'
+    username: "Aniket",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(`Username: ${this.loginCount}`);
+        
+        // console.log(this);
+    }
+
 }
 
-console.log(user.username);
+
+
+console.log(user.username)
+console.log(user.getUserDetails());
+// console.log(this);
+
